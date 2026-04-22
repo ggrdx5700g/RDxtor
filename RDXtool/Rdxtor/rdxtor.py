9a59ddfd940dc9cd53581d2b67dfea20ae82232c
ggrdx5700g <ggrdx5700g@gmail.com>
2026-04-22 09:45:28 +0600

    Update user prompts and instructions in rdxtor.py

--- RDXtool/Rdxtor/rdxtor.py
+++ RDXtool/Rdxtor/rdxtor.py
@@ -7,14 +7,12 @@ print(text)
 print('---------------------')
 
 options = [
-    '1. enter to tor browser'
+    'Entering to tor browser...'
 ]
 print(options)
-
-print('comment beside strict_chain and comment out beside dynamic_chain and comment out proxy_dns.. then scroll down and you will find a proxylist and use socks5 under socks4 and everything should similiar , then do ctrl-x , y and enter and If it give error run {sudo apt install tor}')
 print('- - - - - -  - - - - - - -- - - -- --')
 
-o = input('continue Y: ')
+o = input('continue [Y/y]: ')
 if o.lower() == 'y':
     subprocess.call('sudo nano /etc/proxychains4.conf', shell=True)
     subprocess.call('sudo service tor start', shell=True)
